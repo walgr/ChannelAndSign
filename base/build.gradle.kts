4c83d0c70290384ade4263ef8da1da01dcc97e0a
王朋飞 <walgr1010>
2023-06-27 17:04:23 +0800

    优化代码。

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     implementation(project(":AXMLEditor3"))
-    implementation("com.android:zipflinger:8.0.0")              //压缩包操作
+    implementation("com.android:zipflinger:7.3.1")              //压缩包操作
 //    implementation("co.touchlab:kermit:2.0.0-RC4")            //日志保存
     implementation(kotlin("stdlib-jdk8"))
 }
