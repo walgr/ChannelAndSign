688b794c5fca90e03d85a09046921d7cb63d35cf
王朋飞 <wpf6457@qq.com>
2024-04-24 14:41:12 +0800

    优化功能。

--- common/src/commonMain/kotlin/com/wpf/util/common/ui/jiagu/jiaguPage.kt
+++ common/src/commonMain/kotlin/com/wpf/util/common/ui/jiagu/jiaguPage.kt
@@ -35,6 +35,8 @@ import com.wpf.util.common.ui.widget.common.Title
 import com.wpf.utils.curPath
 import com.wpf.utils.ex.createCheck
 import com.wpf.utils.jiagu.Jiagu
+import com.wpf.utils.jiagu.utils.AES128Helper
+import com.wpf.utils.jiagu.utils.AES128Helper.KEY_VI
 import com.wpf.utils.jiagu.utils.RSAUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -66,159 +68,139 @@ fun jiaguPage() {
                     ) {
                         Text("加固", fontWeight = FontWeight.Bold, color = mainTextColor)
                     }
-                    Box(
-                        modifier = Modifier.fillMaxWidth().wrapContentHeight()
-                            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                            .clip(shape = RoundedCornerShape(8.dp)).background(color = centerBgColor),
-                    ) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
+                    Row {
+                        Box(
+                            modifier = Modifier.weight(2f).fillMaxHeight()
+                                .padding(start = 16.dp, end = 8.dp, bottom = 16.dp)
+                                .clip(shape = RoundedCornerShape(8.dp)).background(color = centerBgColor),
                         ) {
-                            Row(
-                                modifier = Modifier.wrapContentWidth(),
-                                verticalAlignment = Alignment.CenterVertically
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
                             ) {
-                                Title("公私钥设置", modifier = Modifier.wrapContentWidth().height(44.dp))
-                            }
-                            Row {
                                 Row(
-                                    modifier = Modifier.weight(1f).padding(end = 8.dp),
+                                    modifier = Modifier.wrapContentWidth(),
                                     verticalAlignment = Alignment.CenterVertically
                                 ) {
-                                    InputView(
-                                        modifier = Modifier.weight(1f),
-                                        input = inputPrivateKeyFilePath,
-                                        hint = "请输入私钥位置"
+                                    Title("密钥设置", modifier = Modifier.wrapContentWidth().height(44.dp))
+                                }
+                                Column {
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+                                        verticalAlignment = Alignment.CenterVertically
                                     ) {
-                                        inputPrivateKeyFilePath.value = it
-                                    }
-                                    Button(onClick = {
-                                        FileSelector.showFileSelector(arrayOf("key", "cer", "crt")) {
+                                        InputView(
+                                            modifier = Modifier.weight(1f),
+                                            input = inputPrivateKeyFilePath,
+                                            hint = "请输入密钥"
+                                        ) {
                                             inputPrivateKeyFilePath.value = it
                                         }
-                                    }, modifier = Modifier.padding(start = 8.dp)) {
-                                        Text("选择")
                                     }
-                                }
 
-                                Row(
-                                    modifier = Modifier.weight(1f).padding(end = 8.dp),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    InputView(
-                                        modifier = Modifier.weight(1f),
-                                        input = inputPublicKeyFilePath,
-                                        hint = "请输入公钥位置"
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth().padding(start = 16.dp, top = 8.dp, end = 16.dp),
+                                        verticalAlignment = Alignment.CenterVertically
                                     ) {
-                                        inputPublicKeyFilePath.value = it
-                                    }
-                                    Button(onClick = {
-                                        FileSelector.showFileSelector(arrayOf("cer", "crt")) {
+                                        InputView(
+                                            modifier = Modifier.weight(1f),
+                                            input = inputPublicKeyFilePath,
+                                            hint = "请输入密钥IV"
+                                        ) {
                                             inputPublicKeyFilePath.value = it
                                         }
-                                    }, modifier = Modifier.padding(start = 8.dp)) {
-                                        Text("选择")
                                     }
                                 }
-                            }
-                            Button(onClick = {
-                                val keyPair = RSAUtil.generateKeyPair()
-                                val privateKeyFile =
-                                    File(curPath + File.separator + "rsa" + File.separator + "privateKey.key").createCheck(
-                                        true
-                                    )
-                                val publicKeyFile =
-                                    File(curPath + File.separator + "rsa" + File.separator + "publicKey.cer").createCheck(
-                                        true
-                                    )
-                                privateKeyFile.writeText(keyPair.privateKey)
-                                publicKeyFile.writeText(keyPair.publicKey)
-                                inputPrivateKeyFilePath.value = privateKeyFile.path
-                                inputPublicKeyFilePath.value = publicKeyFile.path
-                            }, modifier = Modifier.padding(start = 8.dp)) {
-                                Text("生成新公私钥")
+                                Button(onClick = {
+                                    AES128Helper.DEFAULT_SECRET_KEY = AES128Helper.getRandom(16)
+                                    KEY_VI = AES128Helper.getRandom(16)
+                                    inputPrivateKeyFilePath.value = AES128Helper.DEFAULT_SECRET_KEY
+                                    inputPublicKeyFilePath.value = KEY_VI
+                                }, modifier = Modifier.padding(top = 8.dp)) {
+                                    Text("生成新密钥IV")
+                                }
                             }
                         }
-                    }
-                    Box(
-                        modifier = Modifier.weight(1f).fillMaxWidth()
-                            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                            .clip(shape = RoundedCornerShape(8.dp)).background(color = centerBgColor),
-                    ) {
-                        Column {
-                            SelectFileAddTitle("文件列表", arrayOf("apk")) {
-                                dealFileList.add(Path(it, it))
-                            }
-                            Box(modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp)) {
-                                LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                                    items(dealFileList) {
-                                        ItemView(modifier = Modifier.heightIn(min = 24.dp)
-                                            .combinedClickable(onDoubleClick = {
-                                                dealFileList.remove(it)
-                                            }) {}) {
-                                            Text(
-                                                it.path,
-                                                fontSize = 11.sp,
-                                                color = Color.DarkGray,
-                                                modifier = Modifier.padding(
-                                                    start = 8.dp, end = 8.dp
+
+                        Box(
+                            modifier = Modifier.weight(1f).fillMaxHeight()
+                                .padding(start = 8.dp, end = 16.dp, bottom = 16.dp)
+                                .clip(shape = RoundedCornerShape(8.dp)).background(color = centerBgColor),
+                        ) {
+                            Column {
+                                SelectFileAddTitle("文件列表", arrayOf("apk")) {
+                                    dealFileList.add(Path(it, it))
+                                }
+                                Box(modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp)) {
+                                    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                                        items(dealFileList) {
+                                            ItemView(modifier = Modifier.heightIn(min = 24.dp)
+                                                .combinedClickable(onDoubleClick = {
+                                                    dealFileList.remove(it)
+                                                }) {}) {
+                                                Text(
+                                                    it.path,
+                                                    fontSize = 11.sp,
+                                                    color = Color.DarkGray,
+                                                    modifier = Modifier.padding(
+                                                        start = 8.dp, end = 8.dp
+                                                    )
                                                 )
+                                            }
+                                        }
+                                    }
+                                    Box(
+                                        modifier = Modifier.fillMaxSize(),
+                                        contentAlignment = Alignment.BottomEnd
+                                    ) {
+                                        IconButton(onClick = {
+                                            if (isRunDealFile) return@IconButton
+                                            if (dealFileList.isEmpty()) {
+
+                                            } else {
+                                                isRunDealFile = true
+                                                val apkPathList = dealFileList.flatMap {
+                                                    val pathFile = File(it.path)
+                                                    if (pathFile.isFile && pathFile.extension == "apk") {
+                                                        listOf(it.path)
+                                                    } else if (pathFile.isDirectory) {
+                                                        pathFile.listFiles { child -> pathFile.isFile && child.extension == "apk" }
+                                                            ?.map { child ->
+                                                                child.path
+                                                            }?.toList() ?: emptyList()
+                                                    } else {
+                                                        emptyList()
+                                                    }
+                                                }
+                                                if (apkPathList.isEmpty()) return@IconButton
+                                                CoroutineScope(Dispatchers.Default).launch {
+                                                    apkPathList.forEach {
+                                                        Jiagu.deal(
+                                                            it,
+                                                            privateKeyFilePath = inputPrivateKeyFilePath.value,
+                                                            publicKeyFilePath = inputPublicKeyFilePath.value,
+                                                            showLog = true
+                                                        )
+                                                    }
+                                                    isRunDealFile = false
+                                                }
+                                            }
+                                        }) {
+                                            Icon(
+                                                if (isRunDealFile) Icons.Default.Close else Icons.Default.PlayArrow,
+                                                "开始加固"
                                             )
                                         }
                                     }
                                 }
-                                Box(
-                                    modifier = Modifier.fillMaxSize(),
-                                    contentAlignment = Alignment.BottomEnd
-                                ) {
-                                    IconButton(onClick = {
-                                        if (isRunDealFile) return@IconButton
-                                        if (dealFileList.isEmpty()) {
-
-                                        } else {
-                                            isRunDealFile = true
-                                            val apkPathList = dealFileList.flatMap {
-                                                val pathFile = File(it.path)
-                                                if (pathFile.isFile && pathFile.extension == "apk") {
-                                                    listOf(it.path)
-                                                } else if (pathFile.isDirectory) {
-                                                    pathFile.listFiles { child -> pathFile.isFile && child.extension == "apk" }
-                                                        ?.map { child ->
-                                                            child.path
-                                                        }?.toList() ?: emptyList()
-                                                } else {
-                                                    emptyList()
-                                                }
-                                            }
-                                            if (apkPathList.isEmpty()) return@IconButton
-                                            CoroutineScope(Dispatchers.Default).launch {
-                                                apkPathList.forEach {
-                                                    Jiagu.deal(
-                                                        it,
-                                                        privateKeyFilePath = inputPrivateKeyFilePath.value,
-                                                        publicKeyFilePath = inputPublicKeyFilePath.value,
-                                                        showLog = true
-                                                    )
-                                                }
-                                                isRunDealFile = false
-                                            }
-                                        }
-                                    }) {
-                                        Icon(
-                                            if (isRunDealFile) Icons.Default.Close else Icons.Default.PlayArrow,
-                                            "开始加固"
-                                        )
-                                    }
-                                }
                             }
-                        }
-                        onExternalDrag {
-                            dealFileList.addAll(it.flatMap { file ->
-                                //apk或文件夹
-                                if (file.contains(".apk") || !file.contains(".")) {
-                                    mutableListOf(Path(name = file, path = file))
-                                } else mutableListOf()
-                            }.toMutableList())
+                            onExternalDrag {
+                                dealFileList.addAll(it.flatMap { file ->
+                                    //apk或文件夹
+                                    if (file.contains(".apk") || !file.contains(".")) {
+                                        mutableListOf(Path(name = file, path = file))
+                                    } else mutableListOf()
+                                }.toMutableList())
+                            }
                         }
                     }
                 }
